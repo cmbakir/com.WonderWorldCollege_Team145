64342bbf938fc0b0c5f164f7f3791eaab39300a3
pinarcay <pinarcay1984@gmail.com>
2025-01-10 17:02:12 +0300

    pinar/10.01.2025/StudentPage_PinarEklendi

---
+++ src/test/java/pages/StudentPage_Pinar.java
@@ -0,0 +1,65 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+import java.util.List;
+
+public class StudentPage_Pinar {
+
+    public StudentPage_Pinar(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy (xpath = "//*[@class='fa fa-flask ftlayer']")
+    public WebElement studentHwMenu;
+
+    @FindBy (xpath = "(//*[@class='active'])[2]")
+    public WebElement upcommingHwMenu;
+
+    //@FindBy (xpath = "(//*[@role='row'])[1]")
+    //public List<WebElements> upcommingHwTitles;
+
+    @FindBy (xpath = "//*[@class='fa fa-reorder']")
+    public WebElement upcommingActionButton;
+
+    @FindBy (xpath = "//*[@name='message']")
+    public WebElement hwDetailsMessageBox;
+
+    @FindBy (xpath = "//*[@id='submit']")
+    public WebElement hwDetailsSaveButton;
+
+    @FindBy (xpath = "//*[text()='Homework Details']")
+    public WebElement upcommingActionHwDetailsMenu;
+
+    @FindBy (xpath = "//*[@class='closed-exam']")
+    public WebElement closedHwMenu;
+
+    @FindBy (xpath = "//*[@class='box-tools pull-right']")
+    public WebElement dailyAssignmentButton;
+
+    @FindBy (xpath = "//*[@class='btn btn-sm btn-primary']")
+    public WebElement addDailyAssignmentButton;
+
+    @FindBy (xpath = "//*[@id='subject']")
+    public WebElement addDailyAssignSubjectButton;
+
+    @FindBy (xpath = "//*[@id='title']")
+    public WebElement addDailyAssignTitleButton;
+
+    @FindBy (xpath = "(//*[text()='Save'])[1]")
+    public WebElement addDailyAssignSaveButton;
+
+    @FindBy (xpath = "(//*[@role='row'])[2]")
+    public WebElement firstRowOfAssignList;
+
+
+
+
+
+
+
+
+}
