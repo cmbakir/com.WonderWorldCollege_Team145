969b43fb8341c5a8a019cd0e1d2593486089ccb8
slymanvar <suleymanvarol578@gmail.com>
2025-01-12 23:36:52 +0100

    updatesuleymanv1

--- src/test/java/pages/TeacherAttendancePage.java
+++ src/test/java/pages/TeacherAttendancePage.java
@@ -1,5 +1,7 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
@@ -7,7 +9,8 @@ public class TeacherAttendancePage {
 
     public TeacherAttendancePage(){PageFactory.initElements(Driver.getDriver(), this);}
 
-
+    @FindBy(xpath = "(//*[@class=\"fa fa-search\"])[3]")
+    public WebElement perAttDateTitleText;
 
 
 
