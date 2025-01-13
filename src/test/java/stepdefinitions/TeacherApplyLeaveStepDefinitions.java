964b798c4e5091eb67a6dce3a22aa1474ddac830
cembakir <cembakir@hotmail.com>
2025-01-13 16:58:31 +0300

    TeacherApplyLeaveStepDefinitions Updated

--- src/test/java/stepdefinitions/TeacherApplyLeaveStepDefinitions.java
+++ src/test/java/stepdefinitions/TeacherApplyLeaveStepDefinitions.java
@@ -2,7 +2,9 @@ package stepdefinitions;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.support.ui.Select;
 import pages.TeacherApplyLeavePage;
 import utilities.Driver;
 import utilities.ReusableMethods;
@@ -39,8 +41,8 @@ public class TeacherApplyLeaveStepDefinitions {
         String ActualUrlApplyLeave = Driver.getDriver().getCurrentUrl();
 
        Assertions.assertEquals(ExpectedUrlApplyLeave,ActualUrlApplyLeave);
-       System.out.println(ExpectedUrlApplyLeave);
-       System.out.println(ActualUrlApplyLeave);
+       //System.out.println(ExpectedUrlApplyLeave);
+       //System.out.println(ActualUrlApplyLeave);
 
     }
 
@@ -53,12 +55,15 @@ public class TeacherApplyLeaveStepDefinitions {
 
     @Then("In the opened Add Details window, the fields Apply Date, Available Leave, Leave From Date, Leave To Date, Reason box, and the Save button should be visible and active.")
     public void in_the_opened_add_details_window_the_fields_apply_date_available_leave_leave_from_date_leave_to_date_reason_box_and_the_save_button_should_be_visible_and_active() {
+
         String popUpWindowWH = Driver.getDriver().getWindowHandle();
         Driver.getDriver().switchTo().window(popUpWindowWH);
-        teacherApplyLeavePage.dateInputLeaveFromDate.sendKeys("03/01/2025");
-        ReusableMethods.bekle(6);
+        Assertions.assertTrue(teacherApplyLeavePage.labelApplyDate.isDisplayed());
         Assertions.assertTrue(teacherApplyLeavePage.dateInputLeaveFromDate.isDisplayed());
-
+        Assertions.assertTrue(teacherApplyLeavePage.dateInputLeaveToDate.isDisplayed());
+        Assertions.assertTrue(teacherApplyLeavePage.dropDownAvailableLeave.isDisplayed());
+        Assertions.assertTrue(teacherApplyLeavePage.inputBoxReason.isDisplayed());
+        Assertions.assertTrue(teacherApplyLeavePage.saveButtonApplyLeave.isDisplayed());
 
 
     }
@@ -66,5 +71,104 @@ public class TeacherApplyLeaveStepDefinitions {
     @Then("In the Add Details window, the user fills in the required fields, clicks the Save button, and creates a new leave record.")
     public void in_the_add_details_window_the_user_fills_in_the_required_fields_clicks_the_save_button_and_creates_a_new_leave_record() {
 
+        teacherApplyLeavePage.dropDownAvailableLeave.click();
+        ReusableMethods.bekle(2);
+        //teacherApplyLeavePage.dropDownAvailableLeave.sendKeys("Casual Leave (20)");
+        Select select = new Select(teacherApplyLeavePage.dropDownAvailableLeave);
+        select.selectByValue("2");
+
+
+        // teacherApplyLeavePage.dateInputLeaveToDate.click();
+        teacherApplyLeavePage.dateInputLeaveFromDate.click();
+        ReusableMethods.bekle(2);
+        teacherApplyLeavePage.dateChoiceLeaveFromDate.click();
+        // teacherApplyLeavePage.dateInputLeaveFromDate.sendKeys("20/01/2025");
+        // ReusableMethods.bekle(1);
+        // System.out.println(teacherApplyLeavePage.dateInputLeaveToDate.getText());
+        teacherApplyLeavePage.dateInputLeaveToDate.click();
+        ReusableMethods.bekle(2);
+        teacherApplyLeavePage.dateChoiceLeaveToDate.click();
+        // System.out.println(teacherApplyLeavePage.dateInputLeaveFromDate);
+
+        // teacherApplyLeavePage.dateInputLeaveFromDate.sendKeys("20/01/2025");
+        // ReusableMethods.bekle(5);
+
+        teacherApplyLeavePage.inputBoxReason.sendKeys("They will deliver the new car I will purchase.");
+        ReusableMethods.bekle(2);
+
+        teacherApplyLeavePage.saveButtonApplyLeave.click();
+        ReusableMethods.bekle(6);
+
+        Assertions.assertTrue(teacherApplyLeavePage.row1stLeave.isDisplayed());
+
+        //teacherApplyLeavePage.row1stLeave.isDisplayed();
+        //System.out.println(teacherApplyLeavePage.row1stLeave.getText());
+        Assertions.assertTrue(teacherApplyLeavePage.row1stLeave.getText().contains("Leave"));
+
     }
-}
+    @When("All created leave records are displayed in the Leaves List with the headings Staff, Leave Type, Leave Date, Days, Apply Date, Status, and Action.")
+        public void all_created_leave_records_are_displayed_in_the_leaves_list_with_the_headings_staff_leave_type_leave_date_days_apply_date_status_and_action() {
+        Assertions.assertTrue(teacherApplyLeavePage.labelStaffAtLeaves.isDisplayed());
+        Assertions.assertTrue(teacherApplyLeavePage.labelLeaveTypeAtLeaves.isDisplayed());
+        Assertions.assertTrue(teacherApplyLeavePage.labelLeaveDateAtLeaves.isDisplayed());
+        Assertions.assertTrue(teacherApplyLeavePage.labelDaysAtLeaves.isDisplayed());
+        Assertions.assertTrue(teacherApplyLeavePage.labelApplyDateAtLeaves.isDisplayed());
+        Assertions.assertTrue(teacherApplyLeavePage.labelStatusAtLeaves.isDisplayed());
+        Assertions.assertTrue(teacherApplyLeavePage.labelActionAtLeaves.isDisplayed());
+
+        }
+    @Then("It is verified that the status of the leave record is displayed under the Status heading in the Leaves List.")
+        public void it_is_verified_that_the_status_of_the_leave_record_is_displayed_under_the_status_heading_in_the_leaves_list() {
+
+        }
+    @Then("Under the Action heading in the Leaves List, the delete icon is displayed for leave records with a status of pending.")
+        public void under_the_action_heading_in_the_leaves_list_the_delete_icon_is_displayed_for_leave_records_with_a_status_of_pending() {
+
+        }
+   @Then("the icon under the Action heading is clicked, the corresponding record is deleted.")
+        public void the_icon_under_the_action_heading_is_clicked_the_corresponding_record_is_deleted() {
+        teacherApplyLeavePage.iconDeleteAtLeaves.click();
+        Driver.getDriver().switchTo().alert().accept();
+
+
+        }
+   @Then("The details window of the selected leave record can be accessed by clicking the view icon under the Action heading.")
+        public void the_details_window_of_the_selected_leave_record_can_be_accessed_by_clicking_the_view_icon_under_the_action_heading() {
+
+       teacherApplyLeavePage.applyLeaveButton.click();
+       ReusableMethods.bekle(2);
+       String popUpWindowNewWH = Driver.getDriver().getWindowHandle();
+       Driver.getDriver().switchTo().window(popUpWindowNewWH);
+
+       teacherApplyLeavePage.dropDownAvailableLeave.click();
+       ReusableMethods.bekle(2);
+
+       Select select = new Select(teacherApplyLeavePage.dropDownAvailableLeave);
+       select.selectByValue("2");
+
+       teacherApplyLeavePage.dateInputLeaveFromDate.click();
+       ReusableMethods.bekle(2);
+       teacherApplyLeavePage.dateChoiceLeaveFromDate.click();
+
+       teacherApplyLeavePage.dateInputLeaveToDate.click();
+       ReusableMethods.bekle(2);
+       teacherApplyLeavePage.dateChoiceLeaveToDate.click();
+
+       teacherApplyLeavePage.inputBoxReason.sendKeys("They will deliver the new car I will purchase.");
+       ReusableMethods.bekle(2);
+
+       teacherApplyLeavePage.saveButtonApplyLeave.click();
+       ReusableMethods.bekle(6);
+
+
+        teacherApplyLeavePage.iconViewAtLeaves.click();
+        ReusableMethods.bekle(4);
+       String popUpViewWindowWH = Driver.getDriver().getWindowHandle();
+       Driver.getDriver().switchTo().window(popUpViewWindowWH);
+       Assertions.assertTrue(teacherApplyLeavePage.tableViewAtLeaves.isDisplayed());
+       Assertions.assertTrue(teacherApplyLeavePage.tableViewAtLeaves.getText().contains("Details"));
+
+        }
+
+
+    }
