dd036ffc4904380e6f86315714e860287cab2c9b
slymanvar <suleymanvarol578@gmail.com>
2025-01-13 14:46:10 +0100

    suleyman/13.01.2025/TeacherAttendancePage guncellendi

--- src/test/java/pages/TeacherAttendancePage.java
+++ src/test/java/pages/TeacherAttendancePage.java
@@ -9,12 +9,40 @@ public class TeacherAttendancePage {
 
     public TeacherAttendancePage(){PageFactory.initElements(Driver.getDriver(), this);}
 
-    @FindBy(xpath = "(//*[@class=\"fa fa-search\"])[3]")
+    @FindBy(xpath = "//*[@class=\"box-title\"]")
     public WebElement perAttDateTitleText;
     @FindBy(xpath = "(//*[@class=\"fa fa-calendar-check-o ftlayer\"])[2]")
     public WebElement attendanceButton;
+    @FindBy(xpath = "(//*[@href=\"https://qa.wonderworldcollege.com/admin/subjectattendence/reportbydate\"])[2]")
+    public WebElement PerAttDateMenu;
+    @FindBy(xpath = "(//*[@for=\"exampleInputEmail1\"])[1]")
+    public WebElement selCrClassText;
+    @FindBy(xpath = "//*[@name=\"class_id\"]")
+    public WebElement selCrClassDropdown;
+    @FindBy(xpath = "(//*[@for=\"exampleInputEmail1\"])[2]")
+    public WebElement selCrSectionText;
+    @FindBy(xpath = "//*[@name=\"section_id\"]")
+    public WebElement selCrSectionDropdown;
+    @FindBy(xpath = "(//*[@for=\"exampleInputEmail1\"])[3]")
+    public WebElement selCrDateText;
+    @FindBy(xpath = "//*[@name=\"date\"]")
+    public WebElement selCrDateBox;
+    @FindBy(xpath = "(//*[@name=\"search\"])[3]")
+    public WebElement selCrSearchText;
+    @FindBy(xpath = "(//*[@class=\"fa fa-search\"])[4]")
+    public WebElement selCrSearchButton;
 
+    @FindBy(xpath = "//*[@class=\"table-responsive\"]")
+    public WebElement studentListForm;
 
+    @FindBy(xpath = "(//th[\"Student\"])[1]")
+    public WebElement StudentListTitle;
+
+    @FindBy(xpath = "(//*[@class=\"text text-center\"])[1]")
+    public WebElement studListDersTitleText;
+
+    @FindBy(xpath = "(//*[@class=\"day\"])[8]")
+    public WebElement dateTableBoxText;
 
 
 
